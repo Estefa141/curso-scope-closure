b4ef44d6e7b152b4a1410e706a79e56c1a3ec72b
Estefania Valencia <este.fa.141@hotmail.es>
2022-08-22 07:54:22 -0500

    Strict mode

---
+++ src/scope/strict.js
@@ -0,0 +1,14 @@
+pi = 3.1416; //Asignar sin declarar
+console.log(pi);
+
+'use strict';
+pi = 3.1416; //Asignar sin declarar
+console.log(pi);
+//pide que declare, que defina antes de asignar
+
+function myFunction() {
+    'use strict';
+    return pi = 3.1416;//Asignar sin declarar
+}
+console.log(myFunction());
+//pide que declare, que defina antes de asignar
